caa18a096da3c8e7b17e53747976d3aa106e9409
thyeone <thyeonee@gmail.com>
2022-11-11 19:50:59 +0900

    숫자 비교하기

---
+++ Programmers/Lv0/숫자-비교하기.js
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120807
+
+function solution(num1, num2) {
+  if (num1 == num2) {
+    return 1;
+  } else if (num1 !== num2) {
+    return -1;
+  }
+}
