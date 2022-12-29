6e00738d98396e9d27d8eedda04712b82bea7720
thyeone <thyeonee@gmail.com>
2022-12-29 13:09:17 +0900

    서울에서 김서방 찾기

---
+++ Programmers/Lv1/서울에서-김서방-찾기.js
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12919
+
+function solution(seoul) {
+  const indexOfKim = seoul.indexOf("Kim");
+  return `김서방은 ${indexOfKim}에 있다`;
+}
+
+// seoul = ["Kim"]이 들어간다.
+// Kim의 인덱스를 찾는다.
+// "김서방은 ?에 있다"로 반환
